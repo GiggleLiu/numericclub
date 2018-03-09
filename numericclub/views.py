e8b244cb2134cadc5a623324c734c0038366f709
GiggleLiu <cacate0129@gmail.com>
2018-03-09 15:25:39 +0800

    update cache

--- numericclub/views.py
+++ numericclub/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.cache import cache_page
 import os
 
 from .utils import md2html, html2headercontent
 
-@cache_page(300)
 def help(request, page):
     source_folder, target_folder = 'markdowns', 'templates'
     with open(os.path.join(source_folder, page+'.md'), 'r') as f:
